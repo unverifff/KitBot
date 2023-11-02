ee1d323be4b6160f5383bb81eda569bea87e3f00
Cheeszey2 <125528430+Cheeszey2@users.noreply.github.com>
2023-11-02 20:40:19 +1100

    Got IRL UR3 Working Code

---
+++ IRL TEST Scripts/Endpos.m
@@ -0,0 +1,19 @@
+%%IRL TEST - Motion 2 (End)
+
+currentJointState_321456 = (jointStateSubscriber.LatestMessage.Position)'; % Note the default order of the joints is 3,2,1,4,5,6
+currentJointState_123456 = [currentJointState_321456(3:-1:1),currentJointState_321456(4:6)];
+
+startJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+startJointSend.Positions = currentJointState_123456;
+startJointSend.TimeFromStart = rosduration(0);     
+
+endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint'); 
+% nextJointState_123456 = currentJointState_123456 + [pi/8,0,0,0,0,pi/8];
+nextJointState_123456 = [pi/2, deg2rad(-42), deg2rad(28.5), deg2rad(283), -pi/2, deg2rad(0)];
+endJointSend.Positions = nextJointState_123456;
+endJointSend.TimeFromStart = rosduration(durationSeconds);
+
+goal.Trajectory.Points = [startJointSend; endJointSend];
+
+goal.Trajectory.Header.Stamp = jointStateSubscriber.LatestMessage.Header.Stamp + rosduration(bufferSeconds);
+sendGoal(client,goal);
---
+++ IRL TEST Scripts/IRLMotionPlan.m
@@ -0,0 +1,179 @@
+%% Default
+currentJointState_321456 = (jointStateSubscriber.LatestMessage.Position)'; % Note the default order of the joints is 3,2,1,4,5,6
+currentJointState_123456 = [currentJointState_321456(3:-1:1),currentJointState_321456(4:6)];
+
+[client, goal] = rosactionclient('/scaled_pos_joint_traj_controller/follow_joint_trajectory');
+goal.Trajectory.JointNames = jointNames;
+goal.Trajectory.Header.Seq = 1;
+goal.Trajectory.Header.Stamp = rostime('Now','system');
+goal.GoalTimeTolerance = rosduration(0.05);
+bufferSeconds = 1; % This allows for the time taken to send the message. If the network is fast, this could be reduced.
+durationSeconds = 5; % This is how many seconds the movement will take
+
+startJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+startJointSend.Positions = currentJointState_123456;
+startJointSend.TimeFromStart = rosduration(0);     
+      
+endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+nextJointState_123456 = [0,-pi/2,0,-pi/2,0,0];
+endJointSend.Positions = nextJointState_123456;
+endJointSend.TimeFromStart = rosduration(durationSeconds);
+
+goal.Trajectory.Points = [startJointSend; endJointSend];
+
+goal.Trajectory.Header.Stamp = jointStateSubscriber.LatestMessage.Header.Stamp + rosduration(bufferSeconds);
+sendGoal(client,goal);
+
+input('Continue');
+
+%% Above Basket
+currentJointState_321456 = (jointStateSubscriber.LatestMessage.Position)'; % Note the default order of the joints is 3,2,1,4,5,6
+currentJointState_123456 = [currentJointState_321456(3:-1:1),currentJointState_321456(4:6)];
+
+startJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+startJointSend.Positions = currentJointState_123456;
+startJointSend.TimeFromStart = rosduration(0);     
+      
+endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint'); 
+% nextJointState_123456 = currentJointState_123456 + [pi/8,0,0,0,0,pi/8];
+nextJointState_123456 = [deg2rad(175.3), deg2rad(-61), deg2rad(20), deg2rad(280), deg2rad(-90), deg2rad(0)];
+endJointSend.Positions = nextJointState_123456;
+endJointSend.TimeFromStart = rosduration(durationSeconds);
+
+goal.Trajectory.Points = [startJointSend; endJointSend];
+
+goal.Trajectory.Header.Stamp = jointStateSubscriber.LatestMessage.Header.Stamp + rosduration(bufferSeconds);
+sendGoal(client,goal);
+input('Continue');
+
+%% In Basket
+currentJointState_321456 = (jointStateSubscriber.LatestMessage.Position)'; % Note the default order of the joints is 3,2,1,4,5,6
+currentJointState_123456 = [currentJointState_321456(3:-1:1),currentJointState_321456(4:6)];
+
+startJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+startJointSend.Positions = currentJointState_123456;
+startJointSend.TimeFromStart = rosduration(0);     
+      
+endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint'); 
+% nextJointState_123456 = currentJointState_123456 + [pi/8,0,0,0,0,pi/8];
+nextJointState_123456 = [deg2rad(181.22), deg2rad(-46.98), deg2rad(33.7), deg2rad(282.64), deg2rad(-86.07), deg2rad(8.24)];
+endJointSend.Positions = nextJointState_123456;
+endJointSend.TimeFromStart = rosduration(durationSeconds);
+
+goal.Trajectory.Points = [startJointSend; endJointSend];
+
+goal.Trajectory.Header.Stamp = jointStateSubscriber.LatestMessage.Header.Stamp + rosduration(bufferSeconds);
+sendGoal(client,goal);
+
+input('Continue');
+
+%% Above Basket
+currentJointState_321456 = (jointStateSubscriber.LatestMessage.Position)'; % Note the default order of the joints is 3,2,1,4,5,6
+currentJointState_123456 = [currentJointState_321456(3:-1:1),currentJointState_321456(4:6)];
+
+startJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+startJointSend.Positions = currentJointState_123456;
+startJointSend.TimeFromStart = rosduration(0);     
+      
+endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint'); 
+% nextJointState_123456 = currentJointState_123456 + [pi/8,0,0,0,0,pi/8];
+nextJointState_123456 = [deg2rad(175.3), deg2rad(-61), deg2rad(20), deg2rad(280), deg2rad(-90), deg2rad(0)];
+endJointSend.Positions = nextJointState_123456;
+endJointSend.TimeFromStart = rosduration(durationSeconds);
+
+goal.Trajectory.Points = [startJointSend; endJointSend];
+
+goal.Trajectory.Header.Stamp = jointStateSubscriber.LatestMessage.Header.Stamp + rosduration(bufferSeconds);
+sendGoal(client,goal);
+input('Continue');
+
+%% Above Dining Plate
+currentJointState_321456 = (jointStateSubscriber.LatestMessage.Position)'; % Note the default order of the joints is 3,2,1,4,5,6
+currentJointState_123456 = [currentJointState_321456(3:-1:1),currentJointState_321456(4:6)];
+
+startJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+startJointSend.Positions = currentJointState_123456;
+startJointSend.TimeFromStart = rosduration(0);     
+
+endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint'); 
+% nextJointState_123456 = currentJointState_123456 + [pi/8,0,0,0,0,pi/8];
+nextJointState_123456 = [deg2rad(90), deg2rad(-55), deg2rad(15), deg2rad(295), deg2rad(-90), deg2rad(0)];
+endJointSend.Positions = nextJointState_123456;
+endJointSend.TimeFromStart = rosduration(durationSeconds);
+
+goal.Trajectory.Points = [startJointSend; endJointSend];
+
+goal.Trajectory.Header.Stamp = jointStateSubscriber.LatestMessage.Header.Stamp + rosduration(bufferSeconds);
+sendGoal(client,goal);
+input('Continue');
+
+%% Dining Plate
+currentJointState_321456 = (jointStateSubscriber.LatestMessage.Position)'; % Note the default order of the joints is 3,2,1,4,5,6
+currentJointState_123456 = [currentJointState_321456(3:-1:1),currentJointState_321456(4:6)];
+
+startJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+startJointSend.Positions = currentJointState_123456;
+startJointSend.TimeFromStart = rosduration(0);     
+
+endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint'); 
+% nextJointState_123456 = currentJointState_123456 + [pi/8,0,0,0,0,pi/8];
+nextJointState_123456 = [pi/2, deg2rad(-42), deg2rad(28.5), deg2rad(283), -pi/2, deg2rad(0)];
+endJointSend.Positions = nextJointState_123456;
+endJointSend.TimeFromStart = rosduration(durationSeconds);
+
+goal.Trajectory.Points = [startJointSend; endJointSend];
+
+goal.Trajectory.Header.Stamp = jointStateSubscriber.LatestMessage.Header.Stamp + rosduration(bufferSeconds);
+sendGoal(client,goal);
+input('Continue');
+
+
+%% Above Dining Plate
+currentJointState_321456 = (jointStateSubscriber.LatestMessage.Position)'; % Note the default order of the joints is 3,2,1,4,5,6
+currentJointState_123456 = [currentJointState_321456(3:-1:1),currentJointState_321456(4:6)];
+
+startJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+startJointSend.Positions = currentJointState_123456;
+startJointSend.TimeFromStart = rosduration(0);     
+
+endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint'); 
+% nextJointState_123456 = currentJointState_123456 + [pi/8,0,0,0,0,pi/8];
+nextJointState_123456 = [deg2rad(90), deg2rad(-55), deg2rad(15), deg2rad(295), deg2rad(-90), deg2rad(0)];
+endJointSend.Positions = nextJointState_123456;
+endJointSend.TimeFromStart = rosduration(durationSeconds);
+
+goal.Trajectory.Points = [startJointSend; endJointSend];
+
+goal.Trajectory.Header.Stamp = jointStateSubscriber.LatestMessage.Header.Stamp + rosduration(bufferSeconds);
+sendGoal(client,goal);
+input('Continue');
+
+%% Default
+currentJointState_321456 = (jointStateSubscriber.LatestMessage.Position)'; % Note the default order of the joints is 3,2,1,4,5,6
+currentJointState_123456 = [currentJointState_321456(3:-1:1),currentJointState_321456(4:6)];
+
+[client, goal] = rosactionclient('/scaled_pos_joint_traj_controller/follow_joint_trajectory');
+goal.Trajectory.JointNames = jointNames;
+goal.Trajectory.Header.Seq = 1;
+goal.Trajectory.Header.Stamp = rostime('Now','system');
+goal.GoalTimeTolerance = rosduration(0.05);
+bufferSeconds = 1; % This allows for the time taken to send the message. If the network is fast, this could be reduced.
+durationSeconds = 5; % This is how many seconds the movement will take
+
+startJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+startJointSend.Positions = currentJointState_123456;
+startJointSend.TimeFromStart = rosduration(0);     
+
+endJointSend = rosmessage('trajectory_msgs/JointTrajectoryPoint');
+nextJointState_123456 = [0,-pi/2,0,-pi/2,0,0];
+endJointSend.Positions = nextJointState_123456;
+endJointSend.TimeFromStart = rosduration(durationSeconds);
+
+goal.Trajectory.Points = [startJointSend; endJointSend];
+
+
+goal.Trajectory.Header.Stamp = jointStateSubscriber.LatestMessage.Header.Stamp + rosduration(bufferSeconds);
+sendGoal(client,goal);
+
+
+input('Continue');
