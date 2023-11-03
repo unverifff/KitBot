44b821d82ab3e0d00461e143b54ad30a70ad93ba
Cheeszey2 <125528430+Cheeszey2@users.noreply.github.com>
2023-11-03 23:27:38 +1100

    Forgot to upload

--- moveRobot.m
+++ moveRobot.m
@@ -34,6 +34,17 @@
 %             leave blank if no item is required to be moved
 %         itemIndex -> index within the cell of itemName 
 %             leave blank if no item is required to be moved
+%     moveRobot.RMRC(robotName, time, initQ, nextQ, isRMRC itemName, itemIndex)
+%         robotName -> name of robot
+%         time -> time in seconds for how long movement should take
+%         initPose -> starting cartesian pose
+%         nextPose -> next cartesian pose
+%         isRMRC -> True to turn on RMRC, else put False
+%         itemName -> name of item to move with end effector
+%             leave blank if no item is required to be moved
+%         itemIndex -> index within the cell of itemName 
+%             leave blank if no item is required to be moved
+
 
 classdef moveRobot
     methods(Static)
@@ -241,6 +252,87 @@ classdef moveRobot
                     hold on;
                 end
             % fprintf('animate2');
+        end
+
+        function RMRC(robotName, time, initPose, nextPose, isRMRC, itemName, itemIndex)
+            % Variables
+            deltaT = 0.02; % Control Frequency, steps * deltaT = time(s)
+            steps = time/deltaT; % Number of Steps for Simulation
+            epsilon = 0.1; % Threshold Value for Manipulability
+            s = lspb(0, 1, steps); % Using Trapezoidal Velocity Profile
+            W = diag([0.5 0.5 0.5 1 1 1]); % Weight Matrix for Velocity Vector
+            MoM = zeros(steps,1);             % Array for Measure of Manipulability
+            theta = zeros(3,steps);         % Array for roll-pitch-yaw angles
+            x = zeros(3,steps);             % Array for x-y-z trajectory
+
+            for i = 1:steps
+                x(1,i) = (1 - s(i)) * initPose(1) + s(i) * nextPose(1); % Points in x
+                x(2,i) = (1 - s(i)) * initPose(2) + s(i) * nextPose(2); % Points in y
+                x(3,i) = (1 - s(i)) * initPose(3) + s(i) * nextPose(3); % Points in z
+                theta(1,i) = pi;                 % Roll angle 
+                theta(2,i) = 0;                 % Pitch angle
+                theta(3,i) = 0;                 % Yaw angle
+            end
+            T = [rpy2r(theta(1,1),theta(2,1),theta(3,1)) x(:,1);zeros(1,3) 1]; % Create transformation of first point and angle
+            q0 = zeros(1,7);                                                   % Initial guess for joint angles, 7DOF
+            qMatrix(1,:) = robotName.model.ikcon(T,q0);                            % Solve joint angles to achieve first waypoint
+
+            % RMRC Stuff (based off Lab 9 Solutions)
+            % if isRMRC the for loop
+            if isRMRC == true
+                for p = 1:steps 
+                    J = robotName.model.jacob0(qMatrix(p, :)); % Compute Jacobian at Current Pose
+                    JMod = J;
+                    JMod(:, 1) = []; % Removing the first link (linear rail) from being modified
+                    % MoM = sqrt(det(J * J')); % Measure of Manipulability
+                    MoM(p) = sqrt(det(JMod * JMod')); % Measure of Manipulability
+    
+                    if MoM(p) < epsilon
+                        lambda = (1 - MoM(p)/epsilon) * 5E-2; 
+                    else
+                        lambda = 0;
+                    end
+    
+                    if p < steps
+                        % Compute Desired End Effector Velocity
+                        % deltaX = qMatrix(p+1, :) - qMatrix(p, :);
+                        T1 = robotName.model.fkine(qMatrix(p,:)).T; % Fkine for Current Transformation
+                        T2 = x(:,p+1); % Fkine for Next Transformation
+                        deltaX = T2(1:3, 4) - T1(1:3, 4); % Get Position Error to Waypoint
+                        
+                        Rd = rpy2r(theta(1,p+1),theta(2,p+1),theta(3,p+1)); % Next RPY angles
+                        Ra = T1(1:3,1:3); % Current RPY angles                           
+                        Rdot = (1/deltaT)*(Rd - Ra); % Calculate Rotation Error Matrix 
+                        S = Rdot*Ra'; 
+                        linVel = (1/deltaT)*deltaX;
+                        angVel = [S(3,2);S(1,3);S(2,1)];
+                        xDot = W * [linVel;angVel]; % Calculate End Effector Velocity
+    
+                        % pseudoDLS = J' * (J*J' + lambda*eye(6))^-1; % PseudoDLS Formula
+                        pseudoDLS = JMod' * (JMod*JMod' + lambda*eye(6))^-1; % PseudoDLS Formula
+                        % qDot = pseudoDLS * deltaXDot; % Joint Velocity
+                        qDot = pseudoDLS * xDot; % Joint Velocity
+    
+                        % qMatrix(p+1, :) = qMatrix(p, :) + (qDot' * deltaT) % Integrate qDot to get adjusted qMatrix
+                        qMatrix(p+1, 2:7) = qMatrix(p, 2:7) + (qDot' * deltaT); % Integrate qDot to get adjusted qMatrix
+                    end
+                end
+            end
+
+            % Animate Robot & Item 
+            for o = 1:length(qMatrix)
+                robotName.model.animate(qMatrix(o, :)); % Animate
+        
+                % Checks if itemName is provided to move with End Effector
+                if nargin >= 6 && ~isempty(itemName) && nargin >= 7 && ~isempty(itemIndex) 
+                    endEffectorLocation = robotName.model.fkine(robotName.model.getpos());
+                    offset = transl(0, 0, 0.135) * trotx(pi/2) * troty(0) * trotz(0); % Offset for Gripper
+                    itemName.robotModel{itemIndex}.base = endEffectorLocation.T  * offset;
+                    itemName.robotModel{itemIndex}.animate(0);
+                end
+        
+                drawnow();
+            end
         end
     end
 end
